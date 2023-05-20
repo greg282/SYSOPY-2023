6554c56ed1c916b33c8643f8e98197d5ee78fba5
greg282 <gregor22315@gmail.com>
2023-05-20 18:07:19 +0200

    comments removed

--- cw10/client.c
+++ cw10/client.c
@@ -17,12 +17,14 @@
 int sock;
 int my_id;
 
-void catcher(){
+void catcher()
+{
     while (1)
     {
         struct Message response;
         memset(&response, 0, sizeof(struct Message));
-        if (read(sock, &response, sizeof(struct Message)) == -1) {
+        if (read(sock, &response, sizeof(struct Message)) == -1)
+        {
             perror("read");
             exit(EXIT_FAILURE);
         }
@@ -39,15 +41,14 @@ void catcher(){
             break;
         case STOP:
             printf("STOP\n");
-            //kill parent
             kill(getppid(), SIGINT);
             exit(EXIT_SUCCESS);
             break;
         case PING:
-            //write ping
             response.command_type = PING;
             response.client_id = my_id;
-            if (write(sock, &response, sizeof(struct Message)) == -1) {
+            if (write(sock, &response, sizeof(struct Message)) == -1)
+            {
                 perror("write");
                 exit(EXIT_FAILURE);
             }
@@ -56,15 +57,16 @@ void catcher(){
             break;
         }
     }
-    
 }
-//FUNCTIOIN FOR ATEXIT
-void exit_handler(){
+// FUNCTIOIN FOR ATEXIT
+void exit_handler()
+{
     shutdown(sock, SHUT_RDWR);
     close(sock);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     atexit(exit_handler);
     char name[MAX_NAME_LEN];
     int is_network;
@@ -75,7 +77,8 @@ int main(int argc, char *argv[]) {
     struct sockaddr_un unix_addr;
     char buf[1024];
 
-    if (argc != 4) {
+    if (argc != 4)
+    {
         fprintf(stderr, "Usage: %s <name> <network/local> <server_addr> <server_port/unix_socket_path>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -84,13 +87,15 @@ int main(int argc, char *argv[]) {
     is_network = strcmp(argv[2], "network") == 0;
     server_addr = argv[3];
 
-    if (is_network) {
+    if (is_network)
+    {
         char *port_str = strtok(server_addr, ":");
         char *addr_str = strtok(NULL, ":");
         server_port = atoi(port_str);
 
         sock = socket(AF_INET, SOCK_STREAM, 0);
-        if (sock == -1) {
+        if (sock == -1)
+        {
             perror("socket");
             exit(EXIT_FAILURE);
         }
@@ -98,20 +103,25 @@ int main(int argc, char *argv[]) {
         memset(&addr, 0, sizeof(struct sockaddr_in));
         addr.sin_family = AF_INET;
         addr.sin_port = htons(server_port);
-        if (inet_pton(AF_INET, addr_str, &addr.sin_addr) != 1) {
+        if (inet_pton(AF_INET, addr_str, &addr.sin_addr) != 1)
+        {
             fprintf(stderr, "Invalid address: %s\n", addr_str);
             exit(EXIT_FAILURE);
         }
 
-        if (connect(sock, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) == -1) {
+        if (connect(sock, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)) == -1)
+        {
             perror("connect");
             exit(EXIT_FAILURE);
         }
-    } else {
+    }
+    else
+    {
         unix_socket_path = server_addr;
 
         sock = socket(AF_UNIX, SOCK_STREAM, 0);
-        if (sock == -1) {
+        if (sock == -1)
+        {
             perror("socket");
             exit(EXIT_FAILURE);
         }
@@ -120,7 +130,8 @@ int main(int argc, char *argv[]) {
         unix_addr.sun_family = AF_UNIX;
         strncpy(unix_addr.sun_path, unix_socket_path, sizeof(unix_addr.sun_path) - 1);
 
-        if (connect(sock, (struct sockaddr *) &unix_addr, sizeof(struct sockaddr_un)) == -1) {
+        if (connect(sock, (struct sockaddr *)&unix_addr, sizeof(struct sockaddr_un)) == -1)
+        {
             perror("connect");
             exit(EXIT_FAILURE);
         }
@@ -130,26 +141,30 @@ int main(int argc, char *argv[]) {
     request.client_id = -1;
     request.command_type = INIT;
     strncpy(request.username, name, sizeof(request.username) - 1);
-    snprintf(request.msg ,sizeof(request.msg), "%s has joined the chat\n", name);
-    if (write(sock, &request, sizeof(struct Message)) == -1) {
+    snprintf(request.msg, sizeof(request.msg), "%s has joined the chat\n", name);
+    if (write(sock, &request, sizeof(struct Message)) == -1)
+    {
         perror("write");
         exit(EXIT_FAILURE);
     }
 
     struct Message response_id;
-    if (read(sock, &response_id, sizeof(struct Message)) == -1) {
+    if (read(sock, &response_id, sizeof(struct Message)) == -1)
+    {
         perror("read");
         exit(EXIT_FAILURE);
     }
     printf("Your id is %d\n", response_id.client_id);
     my_id = response_id.client_id;
 
-    void sigint_handler(int sig){
+    void sigint_handler(int sig)
+    {
         request.command_type = STOP;
         request.client_id = -1;
 
-        snprintf(request.msg ,sizeof(request.msg), "%s has left the chat\n", name);
-        if (write(sock, &request, sizeof(struct Message)) == -1) {
+        snprintf(request.msg, sizeof(request.msg), "%s has left the chat\n", name);
+        if (write(sock, &request, sizeof(struct Message)) == -1)
+        {
             perror("write");
             exit(EXIT_FAILURE);
         }
@@ -158,10 +173,12 @@ int main(int argc, char *argv[]) {
     }
 
     pid_t pid = fork();
-    if(pid == 0){
+    if (pid == 0)
+    {
         catcher();
     }
-    else if(pid == -1){
+    else if (pid == -1)
+    {
         perror("fork");
         exit(EXIT_FAILURE);
     }
@@ -169,59 +186,68 @@ int main(int argc, char *argv[]) {
     signal(SIGINT, sigint_handler);
 
     char input[MAX_MESSAGE_LEN];
-    while (1) {
+    while (1)
+    {
         memset(&request, 0, sizeof(struct Message));
         fgets(input, MAX_MESSAGE_LEN, stdin);
-        if(strncmp(input,"LIST",4)==0)
-        {   
-            
+        if (strncmp(input, "LIST", 4) == 0)
+        {
+
             request.command_type = LIST;
             request.client_id = my_id;
-            snprintf(request.msg ,sizeof(request.msg), "LIST\n");
-            
-            
-            if (write(sock, &request, sizeof(struct Message)) == -1) {
+            snprintf(request.msg, sizeof(request.msg), "LIST\n");
+
+            if (write(sock, &request, sizeof(struct Message)) == -1)
+            {
                 perror("write");
                 exit(EXIT_FAILURE);
             }
         }
-         else if (strncmp(input, "2ONE", 4) == 0){
+        else if (strncmp(input, "2ONE", 4) == 0)
+        {
             request.command_type = _2ONE;
             request.client_id = my_id;
 
             sscanf(input, "%*s %d %[^\n]", &request.idTo, request.msg);
-            if(write(sock, &request, sizeof(struct Message)) == -1){
-                perror("write");
-                exit(EXIT_FAILURE);
-            }
-         }
-        else if (strncmp(input, "2ALL", 4) == 0){
-            request.command_type = _2ALL;
-                        request.client_id = my_id;
-
-            sscanf(input, "%*s %[^\n]", request.msg);
-            if(write(sock, &request, sizeof(struct Message)) == -1){
+            if (write(sock, &request, sizeof(struct Message)) == -1)
+            {
                 perror("write");
                 exit(EXIT_FAILURE);
             }
         }
-         else if (strncmp(input, "STOP", 4) == 0){
-            request.command_type = STOP;
-                        request.client_id = my_id;
+        else if (strncmp(input, "2ALL", 4) == 0)
+        {
+            request.command_type = _2ALL;
+            request.client_id = my_id;
 
-            snprintf(request.msg ,sizeof(request.msg), "STOP\n");
-            if (write(sock, &request, sizeof(struct Message)) == -1) {
+            sscanf(input, "%*s %[^\n]", request.msg);
+            if (write(sock, &request, sizeof(struct Message)) == -1)
+            {
                 perror("write");
                 exit(EXIT_FAILURE);
             }
-         }
-         else{
+        }
+        else if (strncmp(input, "STOP", 4) == 0)
+        {
+            request.command_type = STOP;
+            request.client_id = my_id;
+
+            snprintf(request.msg, sizeof(request.msg), "STOP\n");
+            if (write(sock, &request, sizeof(struct Message)) == -1)
+            {
+                perror("write");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
             printf("Wrong command\n");
-         }
+        }
     }
 
     snprintf(buf, sizeof(buf), "%s has left the chat\n", name);
-    if (write(sock, buf, strlen(buf)) == -1) {
+    if (write(sock, buf, strlen(buf)) == -1)
+    {
         perror("write");
         exit(EXIT_FAILURE);
     }
--- cw10/server.c
+++ cw10/server.c
@@ -23,30 +23,32 @@ int tcp_sock, unix_sock;
 
 int id_generator = 0;
 
-
-//mutex
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-struct Client{
+struct Client
+{
     int client_socket;
     int client_id;
     char name[512];
 };
 struct Client client_sockets[MAX_CLIENTS];
 
-void close_all(int sig){
-    for (int i = 0; i < num_clients; i++) {
+void close_all(int sig)
+{
+    for (int i = 0; i < num_clients; i++)
+    {
         struct Message stop;
         stop.command_type = STOP;
         stop.client_id = client_sockets[i].client_id;
-        if (write(client_sockets[i].client_socket, &stop, sizeof(struct Message)) == -1) {
+        if (write(client_sockets[i].client_socket, &stop, sizeof(struct Message)) == -1)
+        {
             perror("write");
             exit(EXIT_FAILURE);
         }
     }
-    for (int i = 0; i < num_clients; i++) {
+    for (int i = 0; i < num_clients; i++)
+    {
         close(client_sockets[i].client_socket);
     }
-
 
     shutdown(unix_socket, SHUT_RDWR);
     shutdown(tcp_sock, SHUT_RDWR);
@@ -56,65 +58,62 @@ void close_all(int sig){
 }
 struct sockaddr_un unix_addr;
 
-void rm_socket(){
+void rm_socket()
+{
     unlink(unix_addr.sun_path);
 }
 
-//CREATE THREAD FUNCTION
-void *thread_function(void *arg){         
+void *thread_function(void *arg)
+{
     while (1)
     {
         sleep(10);
-        //lock 
+
         pthread_mutex_lock(&mutex);
-        //check if clients are still connected
-            //epoll wait
-            //create epoll
-            int epoll_fd = epoll_create1(0);
-            struct epoll_event event;
-            event.events = EPOLLIN|EPOLLEXCLUSIVE;
-            event.data.fd = tcp_sock;
-            
-            if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, tcp_sock, &event) == -1) {
-                perror("epoll_ctl");
+
+        int epoll_fd = epoll_create1(0);
+        struct epoll_event event;
+        event.events = EPOLLIN | EPOLLEXCLUSIVE;
+        event.data.fd = tcp_sock;
+
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, tcp_sock, &event) == -1)
+        {
+            perror("epoll_ctl");
+            exit(EXIT_FAILURE);
+        }
+        event.events = EPOLLIN | EPOLLEXCLUSIVE;
+        event.data.fd = unix_socket;
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, unix_socket, &event) == -1)
+        {
+            perror("epoll_ctl");
+            exit(EXIT_FAILURE);
+        }
+        struct Message ping;
+        ping.command_type = PING;
+        for (int i = 0; i < num_clients; i++)
+        {
+            if (write(client_sockets[i].client_socket, &ping, sizeof(struct Message)) == -1)
+            {
+                perror("write");
                 exit(EXIT_FAILURE);
             }
-            event.events = EPOLLIN|EPOLLEXCLUSIVE;
-            event.data.fd = unix_socket;
-            if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, unix_socket, &event) == -1) {
-                perror("epoll_ctl");
-                exit(EXIT_FAILURE);
-            }
-            //write ping to all clients
-            struct Message ping;
-            ping.command_type = PING;
-            for (int i = 0; i < num_clients; i++) {
-                if (write(client_sockets[i].client_socket, &ping, sizeof(struct Message)) == -1) {
-                    perror("write");
-                    exit(EXIT_FAILURE);
-                }
-            }
-            struct epoll_event events[MAX_EVENTS];
-            int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, 0);
-            if (nfds == -1) {
-                perror("epoll_wait");
-                exit(EXIT_FAILURE);
-            }
-            //read ping from clients if not readed close connection
-           
-        //delete epoll
+        }
+        struct epoll_event events[MAX_EVENTS];
+        int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, 0);
+        if (nfds == -1)
+        {
+            perror("epoll_wait");
+            exit(EXIT_FAILURE);
+        }
+
         epoll_ctl(epoll_fd, EPOLL_CTL_DEL, tcp_sock, NULL);
         epoll_ctl(epoll_fd, EPOLL_CTL_DEL, unix_socket, NULL);
-        //unlock
         pthread_mutex_unlock(&mutex);
-
-        
     }
-    
 }
 
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     atexit(rm_socket);
     struct sockaddr_in tcp_addr;
     int epoll_fd;
@@ -123,14 +122,16 @@ int main(int argc, char *argv[]) {
     int i;
     char buf[1024];
 
-    if (argc != 3) {
+    if (argc != 3)
+    {
         fprintf(stderr, "Usage: %s <tcp_port> <unix_socket>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     tcp_port = atoi(argv[1]);
     unix_socket = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (unix_socket == -1) {
+    if (unix_socket == -1)
+    {
         perror("socket");
         exit(EXIT_FAILURE);
     }
@@ -139,18 +140,21 @@ int main(int argc, char *argv[]) {
     unix_addr.sun_family = AF_UNIX;
     strncpy(unix_addr.sun_path, argv[2], sizeof(unix_addr.sun_path) - 1);
 
-    if (bind(unix_socket, (struct sockaddr *) &unix_addr, sizeof(struct sockaddr_un)) == -1) {
+    if (bind(unix_socket, (struct sockaddr *)&unix_addr, sizeof(struct sockaddr_un)) == -1)
+    {
         perror("bind");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(unix_socket, 5) == -1) {
+    if (listen(unix_socket, 5) == -1)
+    {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
     tcp_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (tcp_sock == -1) {
+    if (tcp_sock == -1)
+    {
         perror("socket");
         exit(EXIT_FAILURE);
     }
@@ -160,18 +164,21 @@ int main(int argc, char *argv[]) {
     tcp_addr.sin_port = htons(tcp_port);
     tcp_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(tcp_sock, (struct sockaddr *) &tcp_addr, sizeof(struct sockaddr_in)) == -1) {
+    if (bind(tcp_sock, (struct sockaddr *)&tcp_addr, sizeof(struct sockaddr_in)) == -1)
+    {
         perror("bind");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(tcp_sock, 5) == -1) {
+    if (listen(tcp_sock, 5) == -1)
+    {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
     epoll_fd = epoll_create1(0);
-    if (epoll_fd == -1) {
+    if (epoll_fd == -1)
+    {
         perror("epoll_create1");
         exit(EXIT_FAILURE);
     }
@@ -179,14 +186,16 @@ int main(int argc, char *argv[]) {
     struct epoll_event event;
     event.events = EPOLLIN;
     event.data.fd = unix_socket;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, unix_socket, &event) == -1) {
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, unix_socket, &event) == -1)
+    {
         perror("epoll_ctl");
         exit(EXIT_FAILURE);
     }
 
     event.events = EPOLLIN;
     event.data.fd = tcp_sock;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, tcp_sock, &event) == -1) {
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, tcp_sock, &event) == -1)
+    {
         perror("epoll_ctl");
         exit(EXIT_FAILURE);
     }
@@ -197,87 +206,99 @@ int main(int argc, char *argv[]) {
     act.sa_flags = 0;
     sigaction(SIGINT, &act, NULL);
 
-    //create thread
     pthread_t thread;
-     pthread_create(&thread, NULL, thread_function, NULL);
+    pthread_create(&thread, NULL, thread_function, NULL);
 
-    while (1) {
-        //clear buf
+    while (1)
+    {
         memset(buf, 0, sizeof(buf));
         nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
-        if (nfds == -1) {
+        if (nfds == -1)
+        {
             perror("epoll_wait");
             exit(EXIT_FAILURE);
         }
-        //lock
         pthread_mutex_lock(&mutex);
 
-        for (i = 0; i < nfds; i++) {
+        for (i = 0; i < nfds; i++)
+        {
 
-            if (events[i].data.fd == unix_socket || events[i].data.fd == tcp_sock) {
+            if (events[i].data.fd == unix_socket || events[i].data.fd == tcp_sock)
+            {
                 int client_sock = accept(events[i].data.fd, NULL, NULL);
-                if (client_sock == -1) {
+                if (client_sock == -1)
+                {
                     perror("accept");
                     continue;
                 }
 
-                
                 char buf[sizeof(struct Message)];
-                ssize_t readed= recv(client_sock, buf, sizeof(struct Message), 0);
-                if(readed < 0){
+                ssize_t readed = recv(client_sock, buf, sizeof(struct Message), 0);
+                if (readed < 0)
+                {
                     printf("Receive failed\n");
                     return 1;
                 }
                 struct Message request;
                 memcpy(&request, buf, sizeof(struct Message));
                 printf("Received message: %s\n", request.msg);
-                
-     
-                if (num_clients >= MAX_CLIENTS) {
+
+                if (num_clients >= MAX_CLIENTS)
+                {
                     fprintf(stderr, "Too many clients\n");
                     close(client_sock);
                     continue;
                 }
 
                 client_sockets[num_clients++].client_socket = client_sock;
-                //client_sockets[num_clients-1].client_id=num_clients-1;
-                client_sockets[num_clients-1].client_id=id_generator++;
-                strcpy(client_sockets[num_clients-1].name, request.username);
+                client_sockets[num_clients - 1].client_id = id_generator++;
+                strcpy(client_sockets[num_clients - 1].name, request.username);
                 struct Message response_id;
-                response_id.client_id = id_generator-1;
-                if (write(client_sock, &response_id, sizeof(struct Message)) == -1) {
+                response_id.client_id = id_generator - 1;
+                if (write(client_sock, &response_id, sizeof(struct Message)) == -1)
+                {
                     perror("write");
                     exit(EXIT_FAILURE);
                 }
 
                 event.events = EPOLLIN;
                 event.data.fd = client_sock;
-                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_sock, &event) == -1) {
+                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_sock, &event) == -1)
+                {
                     perror("epoll_ctl");
                     exit(EXIT_FAILURE);
                 }
-            } else {
+            }
+            else
+            {
                 int client_fd = events[i].data.fd;
                 ssize_t n = read(client_fd, buf, sizeof(buf));
-                if (n == -1) {
+                if (n == -1)
+                {
                     perror("read");
                     exit(EXIT_FAILURE);
-                } else if (n == 0) {
+                }
+                else if (n == 0)
+                {
                     printf("Client disconnected\n");
                     close(client_fd);
-                    for (int j = 0; j < num_clients; j++) {
-                        if (client_sockets[j].client_socket == client_fd) {
+                    for (int j = 0; j < num_clients; j++)
+                    {
+                        if (client_sockets[j].client_socket == client_fd)
+                        {
                             client_sockets[j] = client_sockets[num_clients - 1];
                             num_clients--;
                             break;
                         }
                     }
-                } else { 
+                }
+                else
+                {
                     struct Message received;
                     memcpy(&received, buf, sizeof(struct Message));
-                    if(received.command_type != PING){
-                    printf("Received message: %s\n", received.msg);
-
+                    if (received.command_type != PING)
+                    {
+                        printf("Received message: %s\n", received.msg);
                     }
 
                     switch (received.command_type)
@@ -288,16 +309,19 @@ int main(int argc, char *argv[]) {
                         response_list.client_id = received.client_id;
                         memset(response_list.msg, 0, sizeof(response_list.msg));
                         char temp[1024];
-                        for (int j = 0; j < num_clients; j++) {
+                        for (int j = 0; j < num_clients; j++)
+                        {
                             sprintf(temp, "Client id: %d, name: %s\n", client_sockets[j].client_id, client_sockets[j].name);
                             strcat(response_list.msg, temp);
                         }
-                        
-                        if (write(client_fd, &response_list, sizeof(struct Message)) == -1) {
+
+                        if (write(client_fd, &response_list, sizeof(struct Message)) == -1)
+                        {
                             perror("write");
                             exit(EXIT_FAILURE);
                         }
-                        for (int j = 0; j < num_clients; j++) {
+                        for (int j = 0; j < num_clients; j++)
+                        {
                             printf("Client id: %d, name: %s\n", client_sockets[j].client_id, client_sockets[j].name);
                         }
                         break;
@@ -305,12 +329,15 @@ int main(int argc, char *argv[]) {
                         struct Message response_2one;
                         response_2one.command_type = _2ONE;
                         response_2one.client_id = received.client_id;
-                        
+
                         memset(response_2one.msg, 0, sizeof(response_2one.msg));
                         sprintf(response_2one.msg, "%s", received.msg);
-                        for (int j = 0; j < num_clients; j++) {
-                            if (client_sockets[j].client_id == received.idTo) {
-                                if (write(client_sockets[j].client_socket, &response_2one, sizeof(struct Message)) == -1) {
+                        for (int j = 0; j < num_clients; j++)
+                        {
+                            if (client_sockets[j].client_id == received.idTo)
+                            {
+                                if (write(client_sockets[j].client_socket, &response_2one, sizeof(struct Message)) == -1)
+                                {
                                     perror("write");
                                     exit(EXIT_FAILURE);
                                 }
@@ -324,9 +351,12 @@ int main(int argc, char *argv[]) {
                         response_2all.client_id = received.client_id;
                         memset(response_2all.msg, 0, sizeof(response_2all.msg));
                         sprintf(response_2all.msg, "%s", received.msg);
-                        for (int j = 0; j < num_clients; j++) {
-                            if (client_sockets[j].client_id != received.client_id) {
-                                if (write(client_sockets[j].client_socket, &response_2all, sizeof(struct Message)) == -1) {
+                        for (int j = 0; j < num_clients; j++)
+                        {
+                            if (client_sockets[j].client_id != received.client_id)
+                            {
+                                if (write(client_sockets[j].client_socket, &response_2all, sizeof(struct Message)) == -1)
+                                {
                                     perror("write");
                                     exit(EXIT_FAILURE);
                                 }
@@ -334,10 +364,11 @@ int main(int argc, char *argv[]) {
                         }
                         break;
                     case PING:
-                    //find client
-                        for (int j = 0; j < num_clients; j++) {
-                            if (client_sockets[j].client_id == received.client_id) {
-                        printf("Client id: %d, name: %s is still connected\n", received.client_id, client_sockets[j].name);
+                        for (int j = 0; j < num_clients; j++)
+                        {
+                            if (client_sockets[j].client_id == received.client_id)
+                            {
+                                printf("Client id: %d, name: %s is still connected\n", received.client_id, client_sockets[j].name);
                                 break;
                             }
                         }
@@ -345,14 +376,11 @@ int main(int argc, char *argv[]) {
                     default:
                         break;
                     }
-                    
                 }
             }
         }
-        //unlock
         pthread_mutex_unlock(&mutex);
     }
-
 
     return 0;
 }
